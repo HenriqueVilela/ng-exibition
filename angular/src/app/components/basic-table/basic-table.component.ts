0091dc1252409fe9888b657ba28da42b9627b60e
Henrique vilela <henriquev30@gmail.com>
2024-05-31 14:49:36 +0000

    contextualização da tabela

--- angular/src/app/components/basic-table/basic-table.component.ts
+++ angular/src/app/components/basic-table/basic-table.component.ts
@@ -5,20 +5,19 @@ export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
-  symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Supino reto', weight: 40},
+  {position: 2, name: 'Desenvolvimento', weight: 20},
+  {position: 3, name: 'Cruxifixo', weight: 60},
+  {position: 4, name: 'Puxada lateral', weight: 50},
+  {position: 5, name: 'Puxada frente', weight: 55},
+  {position: 6, name: 'Agachamento com peso', weight: 10},
+  {position: 7, name: 'leg press 45°', weight: 220},
+  {position: 8, name: 'leg press Horizontal', weight: 250},
+  {position: 9, name: 'Panturilha', weight: 15},
+  {position: 10, name: 'Remada cavalinho', weight: 45},
 ];
 
 
@@ -31,7 +30,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class BasicTableComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight'];
   dataSource = ELEMENT_DATA;
 
 }
--- angular/src/app/components/viewport-exemple/viewport-exemple.component.ts
+++ angular/src/app/components/viewport-exemple/viewport-exemple.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { CatCardComponent } from '../../cat-card/cat-card.component';
 import { BasicTableComponent } from '../basic-table/basic-table.component';
 import { LoaderComponent } from '../loader/loader.component';
 import { ItemComponent } from '../item/item.component';
@@ -9,7 +7,7 @@ import { ItemComponent } from '../item/item.component';
 @Component({
   selector: 'app-viewport-exemple',
   standalone: true,
-  imports: [MatGridListModule, ItemComponent, BasicTableComponent, LoaderComponent],
+  imports: [ItemComponent, BasicTableComponent, LoaderComponent],
   templateUrl: './viewport-exemple.component.html',
   styleUrl: './viewport-exemple.component.scss'
 })
